ac4576e69267da99b57a4158219a83d8b8869a5f
krlvm <krlv.mikhail@outlook.com>
2019-12-30 22:41:00 +0300

    Forgotten upper tag check

--- src/ru/krlvm/powertunnel/filter/ProxyFilter.java
+++ src/ru/krlvm/powertunnel/filter/ProxyFilter.java
@@ -59,7 +59,7 @@ public class ProxyFilter extends HttpFiltersAdapter {
         if(httpObject instanceof FullHttpResponse) {
             FullHttpResponse response = ((FullHttpResponse) httpObject);
             String content = response.content().toString(StandardCharsets.UTF_8);
-            if(content.contains("</html>")) {
+            if(content.contains("</html>") || content.contains("</HTML>")) {
                 StringBuilder siteScripts = new StringBuilder();
                 if(complexResponse.getServerHostAndPort() != null) {
                     String server = URLUtility.clearHost(complexResponse.getServerHostAndPort());
