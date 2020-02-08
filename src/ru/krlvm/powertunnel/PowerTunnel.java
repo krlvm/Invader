b32c5d25103921eace4f30d38ff0ee40f6277298
krlvm <51774833+krlvm@users.noreply.github.com>
2020-02-08 21:39:05 +0300

    Fixed the fix from PowerTunnel upstream

--- src/ru/krlvm/powertunnel/PowerTunnel.java
+++ src/ru/krlvm/powertunnel/PowerTunnel.java
@@ -241,13 +241,14 @@ public class PowerTunnel {
     }
 
     public static String safeBootstrap() {
+        String error = null;
         try {
             PowerTunnel.bootstrap();
         } catch (UnknownHostException ex) {
             Utility.print("[x] Cannot use IP-Address '%s': %s", SERVER_IP_ADDRESS, ex.getMessage());
             Debugger.debug(ex);
             Utility.print("[!] Program halted");
-            return "Cannot use IP Address '" + PowerTunnel.SERVER_IP_ADDRESS + "'";
+            error = "Cannot use IP Address '" + PowerTunnel.SERVER_IP_ADDRESS + "'";
         } catch (IOException ex) {
             Utility.print("[x] Something went wrong: " + ex.getMessage());
             ex.printStackTrace();
@@ -255,15 +256,11 @@ public class PowerTunnel {
         } catch (DataStoreException ex) {
             Utility.print("[x] Failed to load data store: " + ex.getMessage());
             ex.printStackTrace();
-            return "Failed to load data store: " + ex.getMessage();
-        } catch (RootCertificateException ex) {
-            Utility.print("[x] Failed to initialize certificate: " + ex.getMessage());
-            ex.printStackTrace();
-            return "Failed to initialize certificate: " + ex.getMessage();
-        } finally {
+            error = "Failed to load data store: " + ex.getMessage();
+        } catch (Exception ex) {
             setStatus(ServerStatus.NOT_RUNNING);
         }
-        return null;
+        return error;
     }
 
     /**
